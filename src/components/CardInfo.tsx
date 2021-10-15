3d20cea6dac2315fe60788ecd3e74ef13400b41f
thayrone <thayronedepaula15@gmail.com>
2021-10-15 11:03:20 -0300

    feat(card): add icons and CarInfo

---
+++ src/components/CardInfo.tsx
@@ -0,0 +1,112 @@
+import {
+  Flex,
+  Box,
+  List,
+  ListItem,
+  Text,
+  Divider,
+  useColorMode,
+} from "@chakra-ui/react";
+import CalendarIcon from "../../public/icons/calendar.svg";
+import MessageIcon from "../../public/icons/message.svg";
+import UserIcon from "../../public/icons/user.svg";
+import CheckIcon from "../../public/icons/check.svg";
+import FlashIcon from "../../public/icons/flash.svg";
+const levelActive = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const num = 9;
+
+export const CardInfo = () => {
+  const { colorMode } = useColorMode();
+
+  const bgColor = { light: "gray.50", dark: "gray.900" };
+
+  const color = { light: "black", dark: "white" };
+  return (
+    <>
+      <Flex gridGap="7px" py="20px">
+        {levelActive.map((item) => {
+          return (
+            <Divider
+              key={item}
+              borderLeftWidth="0"
+              w="3px"
+              h="30px"
+              bg={num > item ? "#02E2AC" : "#02e2ac54"}
+              orientation="vertical"
+            />
+          );
+        })}
+      </Flex>
+      <List color="#9B95B0" fontSize="22px">
+        <ListItem
+          alignItems="center"
+          d="flex"
+          justifyContent="space-between"
+          mb="24px"
+          w="full"
+        >
+          <Box d="flex" alignItems="center">
+            <CalendarIcon />
+            <Text ml="28px">Steward since</Text>
+          </Box>
+          <Text>2021-07-01</Text>
+        </ListItem>
+
+        <ListItem
+          alignItems="center"
+          d="flex"
+          justifyContent="space-between"
+          mb="24px"
+          w="full"
+        >
+          <Box d="flex" alignItems="center">
+            <MessageIcon />
+            <Text ml="28px">Forum post</Text>
+          </Box>
+          <Text>145</Text>
+        </ListItem>
+
+        <ListItem
+          alignItems="center"
+          d="flex"
+          justifyContent="space-between"
+          mb="24px"
+          w="full"
+        >
+          <Box d="flex" alignItems="center" ml='-6px'>
+            <UserIcon  />
+            <Text ml="19px">Delegators</Text>
+          </Box>
+          <Text>719</Text>
+        </ListItem>
+
+        <ListItem
+          alignItems="center"
+          d="flex"
+          justifyContent="space-between"
+          mb="24px"
+          w="full"
+        >
+          <Box d="flex" alignItems="center">
+            <CheckIcon />
+            <Text ml="28px">Voting power</Text>
+          </Box>
+          <Text>10%</Text>
+        </ListItem>
+
+        <ListItem
+          alignItems="center"
+          d="flex"
+          justifyContent="space-between"
+          w="full"
+        >
+          <Box d="flex" alignItems="center">
+            <FlashIcon />
+            <Text ml="28px">Vote participation</Text>
+          </Box>
+          <Text>75%</Text>
+        </ListItem>
+      </List>
+    </>
+  );
+};
--- src/components/CardUser.tsx
+++ src/components/CardUser.tsx
@@ -4,15 +4,14 @@ import {
   Image,
   Heading,
   Link,
-  Divider,
   useColorMode,
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionPanel,
 } from "@chakra-ui/react";
-const levelActive = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const num = 3;
+import { CardInfo } from "./CardInfo";
+
 export const CardUser = () => {
   const { colorMode } = useColorMode();
 
@@ -23,53 +22,59 @@ export const CardUser = () => {
     <Box w="full">
       <Accordion defaultIndex={[1]} allowMultiple>
         <AccordionItem w="590px">
-          <h2>
-            <AccordionButton
-              h="140px"
-              bg="#1A103D"
-              _hover={{ boxShadow: "0px 0px 50px rgba(245, 121, 166, 0.5);" }}
-              p="0"
-            >
-              <Box
-                d="flex"
-                flex="1"
-                textAlign="left"
-                color="white"
-                gridGap="30px"
+          {({ isExpanded }) => (
+            <>
+              <AccordionButton
+                h="140px"
+                bg="#1A103D"
+                _hover={{ boxShadow: "0px 0px 50px rgba(245, 121, 166, 0.5);" }}
+                p="0"
               >
-                <Image
-                  boxSize="140px"
-                  objectFit="cover"
-                  src="https://bit.ly/sage-adebayo"
-                  alt="Segun Adebayo"
-                />
-                <Flex direction="column">
-                  <Heading
-                    as="h2"
-                    fontWeight="semibold"
-                    fontSize="22px"
-                    m="23px 0 7px"
-                  >
-                    Dianne Russell
-                  </Heading>
-                  <Link
-                    fontSize="17px"
-                    color="#9B95B0"
-                    isExternal
-                    href="https://gitcoin.co/username"
-                  >
-                    https://gitcoin.co/username
-                  </Link>
-                </Flex>
-              </Box>
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
+                <Box
+                  d="flex"
+                  flex="1"
+                  textAlign="left"
+                  color="white"
+                  gridGap="30px"
+                >
+                  <Image
+                    boxSize="140px"
+                    objectFit="cover"
+                    src="https://bit.ly/sage-adebayo"
+                    alt="Segun Adebayo"
+                  />
+                  <Flex direction="column">
+                    <Heading
+                      as="h2"
+                      fontWeight="semibold"
+                      fontSize="22px"
+                      m="23px 0 7px"
+                    >
+                      Dianne Russell
+                    </Heading>
+                    <Link
+                      color={isExpanded ? "#5BF1CD" : "#9B95B0"}
+                      fontSize="17px"
+                      href="https://gitcoin.co/username"
+                      _hover={{ textDecoration: "none" }}
+                      isExternal
+                    >
+                      https://gitcoin.co/username
+                    </Link>
+                  </Flex>
+                </Box>
+              </AccordionButton>
+
+              <AccordionPanel
+                bg="#1A103D"
+                p="0 32px 0 28px"
+                h="calc(140px * 3)"
+              >
+               
+                <CardInfo />
+              </AccordionPanel>
+            </>
+          )}
         </AccordionItem>
 
         <AccordionItem>
@@ -81,7 +86,7 @@ export const CardUser = () => {
             </AccordionButton>
           </h2>
           <AccordionPanel pb={4}>
-            <Box d="flex" gridGap="7px">
+            {/* <Box d="flex" gridGap="7px">
               {levelActive.map((item) => {
                 return (
                   <Divider
@@ -93,7 +98,7 @@ export const CardUser = () => {
                   />
                 );
               })}
-            </Box>
+            </Box> */}
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
